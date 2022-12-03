79278af5815d0f88c99fbcb1af679ae49ccf1aa8
echuquillanquiy <edward.axl3@gmail.com>
2022-12-03 11:03:30 -0500

    PARTE ADMINISTRACUION TERMINADA

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>CORK Admin - Multipurpose Bootstrap Dashboard Template </title>
+    <title> HEDIAL </title>
     <link rel="icon" type="image/x-icon" href="{{ asset('theme/assets/img/favicon.ico') }}"/>
     @include('hedial.common.styles')
 
